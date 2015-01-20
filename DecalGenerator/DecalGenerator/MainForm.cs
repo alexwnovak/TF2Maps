b3647d19bf745c828d3d9aa8605a633ee7f10af9
Alex Novak <alexwnovak@gmail.com>
2015-01-19 22:28:44 -0500

    +Stock winforms app

---
+++ DecalGenerator/DecalGenerator/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DecalGenerator
+{
+   public partial class MainForm : Form
+   {
+      public MainForm()
+      {
+         InitializeComponent();
+      }
+   }
+}
