e1fc89d04dc369bef05583982356055573580a51
Alex Novak <alexwnovak@gmail.com>
2015-01-28 12:50:34 -0500

    Now filling the background with transparency and drawing the number
    - Centers the number in the middle of the image
    - Uses Segoe UI and a preset font size
    - Uses a "black brush" that's really black with half opacity

--- DecalGenerator/DecalGenerator/DecalGenerator.cs
+++ DecalGenerator/DecalGenerator/DecalGenerator.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Globalization;
 
 namespace DecalGenerator
 {
@@ -12,8 +15,40 @@ namespace DecalGenerator
          const int borderThickness = 12;
          const float fontSize = 64;
          const float gap = 4;
-         
-         throw new NotImplementedException();
+
+         var bitmap = new Bitmap( width, height );
+         string text = number.ToString( CultureInfo.InvariantCulture );
+
+         using ( Graphics g = Graphics.FromImage( bitmap ) )
+         {
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+            using ( var transparentBrush = new SolidBrush( Color.FromArgb( 0, 0, 0, 0 ) ) )
+            {
+               // Fill the entire image with transparency first
+               // Not sure if necessary... playing it safe since I don't feel like testing it :D
+               g.FillRectangle( transparentBrush, 0, 0, width, height );
+            }
+
+            // Draw the number using Segoe UI (I like the font)
+            // Use a half-opaque black brush to create a see-through dark color
+
+            using ( var blackBrush = new SolidBrush( Color.FromArgb( 128, 0, 0, 0 ) ) )
+            {
+               using ( var font = new Font( "Segoe UI", fontSize ) )
+               {
+                  var textSize = g.MeasureString( text, font );
+
+                  float x = (width - textSize.Width) / 2;
+                  float y = (height - textSize.Height) / 2;
+
+                  g.DrawString( text, font, blackBrush, x, y );
+               }
+            }
+         }
+
+         return bitmap;
       }
    }
 }
