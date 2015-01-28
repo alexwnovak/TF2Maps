c01d3aef8152631358e5eca6cf1a8a25b37b8461
Alex Novak <alexwnovak@gmail.com>
2015-01-28 12:44:59 -0500

    Stubbing out some values we'll use to draw the circle/number

--- DecalGenerator/DecalGenerator/DecalGenerator.cs
+++ DecalGenerator/DecalGenerator/DecalGenerator.cs
@@ -1,6 +1,19 @@
-﻿namespace DecalGenerator
+﻿using System;
+using System.Drawing;
+
+namespace DecalGenerator
 {
    public static class DecalGenerator
    {
+      public static Image Create( int number )
+      {
+         const int width = 256;
+         const int height = 256;
+         const int borderThickness = 12;
+         const float fontSize = 64;
+         const float gap = 4;
+         
+         throw new NotImplementedException();
+      }
    }
 }
