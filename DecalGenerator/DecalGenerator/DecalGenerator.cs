0b21b25a0f21e87c4ad1a828187d90159f54ca0d
Alex Novak <alexwnovak@gmail.com>
2015-01-28 12:55:08 -0500

    Now draws a circle with N-many segments matching the given number
    - Uses the same color as the the number itself
    - Draws N-many "arcs" with interstitial gaps
    - Exaggerates the gaps a bit to give them more breathing room
    - Uses a crazy look-up table to tell each number where on the circle to start
    
    I manually made the look-up table to see what looked best for each number. Note that you'll have to add more of these (or additional smarts) to support larger numbers.
    
    Jump Babylon only has 13 jumps, so I only had to make 13 decals. :D

--- DecalGenerator/DecalGenerator/DecalGenerator.cs
+++ DecalGenerator/DecalGenerator/DecalGenerator.cs
@@ -16,6 +16,11 @@ namespace DecalGenerator
          const float fontSize = 64;
          const float gap = 4;
 
+         float[] startingAngles =
+         {
+            0, 0, 90, 90, 45, 90, 0, 90, 22.5f, 90, 0, 90, 45, 90, 0
+         };
+
          var bitmap = new Bitmap( width, height );
          string text = number.ToString( CultureInfo.InvariantCulture );
 
@@ -44,6 +49,32 @@ namespace DecalGenerator
                   float y = (height - textSize.Height) / 2;
 
                   g.DrawString( text, font, blackBrush, x, y );
+               }
+
+               // Now draw the border, broken into dashes equal to the number we're given
+               // We need a pen this time (not a brush), but can base it on the black brush we already made
+
+               using ( var pen = new Pen( blackBrush, borderThickness ) )
+               {
+                  float sweepAngle = 360.0f / number;
+                  float startAngle = startingAngles[number];
+
+                  if ( number == 1 )
+                  {
+                     // Special case: No dashes here, so just draw a solid circle
+                     g.DrawArc( pen, 16, 16, width - 32, height - 32, 0, 360 );
+                  }
+                  else
+                  {
+                     for ( int index = 0; index < number; index++ )
+                     {
+                        // Modulate the angles a tad to exaggerate the periodic breaks in the circle
+
+                        g.DrawArc( pen, 16, 16, width - 32, height - 32, startAngle + gap, sweepAngle - gap * 2.0f );
+
+                        startAngle += sweepAngle;
+                     }
+                  }
                }
             }
          }
