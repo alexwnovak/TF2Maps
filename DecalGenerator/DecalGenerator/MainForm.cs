16986cfe9a38cf18179927d9b51082bebfafbcfb
Alex Novak <alexwnovak@gmail.com>
2015-01-28 13:03:03 -0500

    Saving the decal that was generated

--- DecalGenerator/DecalGenerator/MainForm.cs
+++ DecalGenerator/DecalGenerator/MainForm.cs
@@ -13,6 +13,10 @@ namespace DecalGenerator
       private void MainForm_Load( object sender, EventArgs e )
       {
          BackgroundImage = DecalGenerator.Create( 10 );
+
+         // Save this wherever you want!
+         // Use it to create more decals!
+         BackgroundImage.Save( "Decal_Number10.png" );
       }
    }
 }
