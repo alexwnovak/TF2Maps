e5f45fb112aa66a41b8093ea6e14e45fdfebd001
Alex Novak <alexwnovak@gmail.com>
2015-11-30 18:53:40 -0500

    Fleshing out glyph rendering
    - Refactoring out the brush/pen/color
    - Adding the new green brush
    - Positions a character around the ring

--- RuneCircleGenerator/RuneCircleGenerator/Generator.cs
+++ RuneCircleGenerator/RuneCircleGenerator/Generator.cs
@@ -6,11 +6,15 @@ namespace RuneCircleGenerator
    public class Generator : IDisposable
    {
       private const int _textureSize = 1024;
+      private const int _symbolCount = 12;
       private static readonly float _penWidth = _textureSize * 0.016f;
+      private static readonly float _fontSize = _textureSize * 0.074f;
 
+      private static Color _greenColor = Color.FromArgb( 255, 156, 255, 134 );
       private SolidBrush _transparentBrush = new SolidBrush( Color.Transparent );
-      private Pen _greenPen = new Pen( Color.FromArgb( 255, 156, 255, 134 ), _penWidth );
-
+      private Pen _greenPen = new Pen( _greenColor, _penWidth );
+      private SolidBrush _greenBrush = new SolidBrush( _greenColor );
+      
       public Image GenerateRuneCircle()
       {
          var bitmap = new Bitmap( _textureSize, _textureSize );
@@ -20,11 +24,39 @@ namespace RuneCircleGenerator
             FillWithTransparency( g );
 
             RenderRings( g );
+
+            RenderSymbols( g );
          }
 
          return bitmap;
       }
 
+      private void RenderSymbols( Graphics g )
+      {
+         float radius = _textureSize / 2 * 0.84f;
+         float centerX = _textureSize / 2;
+         float centerY = _textureSize / 2;
+
+         using ( var font = new Font( "Segoe UI", _fontSize ) )
+         {
+            float theta = 0;
+            char character = 'Z';
+
+            for ( int index = 0; index < _symbolCount; index++ )
+            {
+               var characterSize = MeasureCharacter( g, font, character );
+
+               float x = centerX + (float) Math.Cos( theta ) * radius - characterSize.Width / 2;
+               float y = centerY + (float) Math.Sin( theta ) * radius - characterSize.Height / 2;
+
+               g.DrawString( "A", font, _greenBrush, x, y );
+            }
+         }
+      }
+
+      private SizeF MeasureCharacter( Graphics g, Font font, char c )
+         => g.MeasureString( c.ToString(), font );
+      
       private void RenderRings( Graphics g )
       {
          float outerMargin = _textureSize * 0.02f;
@@ -51,6 +83,7 @@ namespace RuneCircleGenerator
          {
             _transparentBrush?.Dispose();
             _greenPen?.Dispose();
+            _greenBrush?.Dispose();
          }
       }
 
