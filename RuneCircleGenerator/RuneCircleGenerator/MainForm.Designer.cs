28d8dea24af0fc10170be5454db0acd2244ea452
Alex Novak <alexwnovak@gmail.com>
2015-11-30 18:16:26 -0500

    +Initial winforms project

---
+++ RuneCircleGenerator/RuneCircleGenerator/MainForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace RuneCircleGenerator
+{
+   partial class MainForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.SuspendLayout();
+         // 
+         // MainForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+         this.ClientSize = new System.Drawing.Size(1378, 1049);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+         this.MaximizeBox = false;
+         this.MinimizeBox = false;
+         this.Name = "MainForm";
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         this.Text = "Rune Circle Generator";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+   }
+}
+
