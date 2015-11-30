6d2ccbe25cc2dc2d0a9f9e7420f70110fefbddec
Alex Novak <alexwnovak@gmail.com>
2015-11-30 18:41:19 -0500

    Now rendering the rings

--- RuneCircleGenerator/RuneCircleGenerator/Generator.cs
+++ RuneCircleGenerator/RuneCircleGenerator/Generator.cs
@@ -6,8 +6,10 @@ namespace RuneCircleGenerator
    public class Generator : IDisposable
    {
       private const int _textureSize = 1024;
+      private static readonly float _penWidth = _textureSize * 0.016f;
 
       private SolidBrush _transparentBrush = new SolidBrush( Color.Transparent );
+      private Pen _greenPen = new Pen( Color.FromArgb( 255, 156, 255, 134 ), _penWidth );
 
       public Image GenerateRuneCircle()
       {
@@ -16,9 +18,20 @@ namespace RuneCircleGenerator
          using ( var g = Graphics.FromImage( bitmap ) )
          {
             FillWithTransparency( g );
+
+            RenderRings( g );
          }
 
          return bitmap;
+      }
+
+      private void RenderRings( Graphics g )
+      {
+         float outerMargin = _textureSize * 0.02f;
+         float innerMargin = _textureSize * 0.13f;
+
+         g.DrawEllipse( _greenPen, outerMargin, outerMargin, _textureSize - outerMargin * 2, _textureSize - outerMargin * 2 );
+         g.DrawEllipse( _greenPen, innerMargin, innerMargin, _textureSize - innerMargin * 2, _textureSize - innerMargin * 2 );
       }
 
       private void FillWithTransparency( Graphics g )
@@ -37,6 +50,7 @@ namespace RuneCircleGenerator
          if ( disposing )
          {
             _transparentBrush?.Dispose();
+            _greenPen?.Dispose();
          }
       }
 
