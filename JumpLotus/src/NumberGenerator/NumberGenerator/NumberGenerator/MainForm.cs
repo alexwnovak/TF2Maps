a8b88632b03b43eda6d704a10c4626aa385098d4
Alex Novak <alexwnovak@gmail.com>
2016-11-12 15:20:09 -0500

    Now generates and saves the 12 decals as PNGs

--- JumpLotus/src/NumberGenerator/NumberGenerator/NumberGenerator/MainForm.cs
+++ JumpLotus/src/NumberGenerator/NumberGenerator/NumberGenerator/MainForm.cs
@@ -7,6 +7,16 @@ namespace NumberGenerator
       public MainForm()
       {
          InitializeComponent();
+
+         const int jumpCount = 12;
+
+         for ( int index = 1; index <= jumpCount; index++ )
+         {
+            var image = NumberDecalGenerator.Draw( index, 12 );
+
+            string fileName = $@"C:\Temp\JumpLotus\Number{index.ToString( "D2" )}.png";
+            image.Save( fileName );
+         }
       }
    }
 }
