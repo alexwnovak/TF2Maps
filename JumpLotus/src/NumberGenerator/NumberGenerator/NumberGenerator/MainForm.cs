95256035a4a89b69c5c22c8c485dc9f2d10edfc1
Alex Novak <alexwnovak@gmail.com>
2016-11-12 14:08:56 -0500

    +Blank WinForms project
    We'll use this to manually generate the number decals for each jump
    (like the previous maps).

---
+++ JumpLotus/src/NumberGenerator/NumberGenerator/NumberGenerator/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace NumberGenerator
+{
+   public partial class MainForm : Form
+   {
+      public MainForm()
+      {
+         InitializeComponent();
+      }
+   }
+}
