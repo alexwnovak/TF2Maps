daeacbd33ac832a3529094f870d3685d14c47f65
Alex Novak <alexwnovak@gmail.com>
2016-11-12 14:14:23 -0500

    Now creates and returns a plain bitmap of the hard-coded size

--- JumpLotus/src/NumberGenerator/NumberGenerator/NumberGenerator/NumberDecalGenerator.cs
+++ JumpLotus/src/NumberGenerator/NumberGenerator/NumberGenerator/NumberDecalGenerator.cs
@@ -1,6 +1,20 @@
-﻿namespace NumberGenerator
+﻿using System.Drawing;
+
+namespace NumberGenerator
 {
    public static class NumberDecalGenerator
    {
+      private const int _decalSize = 256;
+
+      public static Image Draw( int number, int max )
+      {
+         var bitmap = new Bitmap( _decalSize, _decalSize );
+
+         using ( Graphics g = Graphics.FromImage( bitmap ) )
+         {
+         }
+
+         return bitmap;
+      }
    }
 }
